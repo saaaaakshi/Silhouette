26fb3bee887151d20380d3fea32649578cc57dac
saaaaakshi <109175029+saaaaakshi@users.noreply.github.com>
2023-04-17 22:21:45 +0530

    final template

--- admin/outpass-entry/index.js
+++ admin/outpass-entry/index.js
@@ -1,22 +1,46 @@
+const navbarEl = document.querySelector(".navbar");
+const bottomContainerEl = document.querySelector(".bottom-container");
+// window.addEventListener("scroll",()=>{
+//     if(window.scrollY > bottomContainerEl.offsetTop - navbarEl.offsetHeight - 50){
+//         navbarEl.classList.add("active");
+//     }
+//     else{
+//         navbarEl.classList.remove("active");
+
+//     }
+// });
+
+
+// const sidebtn = document.getElementById("menu-btn");
+// const tableEl = document.getElementById("act");
+const sidebar = document.getElementById("wrapper");
+// if(sidebtn.addEventListener)
+
+// if(sidebtn.addEventListener("click",true)){
+//     sidebar.style.width = "0px";
+//     tableEl.style.marginLeft = "30px";
+// }
+// else{
+//     sidebar.style.width = "30px";
+//     tableEl.style.marginLeft = "280px";
+
+// }
+
+// function openNav() {
+//   document.getElementById("menu-btn").style.width = "250px";
+//   document.getElementById("act").style.marginLeft = "250px";
+// }
+
+// function closeNav() {
+//   document.getElementById("menu-btn").style.width = "0";
+//   document.getElementById("act").style.marginLeft= "0";
+// }
+
+
+
+
 
 const year = document.getElementById("year")
 const thisYear = new Date().getFullYear()
 year.setAttribute("datetime", thisYear)
 year.textContent= thisYear
-
-var rows = $("table tr");
-
-// Loop through each row
-rows.each(function() {
-  // Get the text content of the row
-  var text = $(this).text();
-
-  // Check if the text is empty or not
-  if (text.trim() == "") {
-    // Add the hidden class if it is empty
-    $(this).addClass("hidden");
-  } else {
-    // Remove the hidden class if it has some value
-    $(this).removeClass("hidden");
-  }
-});
---
+++ pop up/index.js
@@ -0,0 +1,7 @@
+
+function toggle(){
+    var blur = document.getElementById("blur");
+    blur.classList.toggle("inact");
+    var popup = document.getElementById("popup");
+    popup.classList.toggle("inact");
+}
